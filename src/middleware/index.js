c49cf20b0ef5a8eb7a530a9f2a395ed292397598
ChanHaoYan1234 <chanhaoyan@126.com>
2021-05-11 12:58:12 +0800

    commit

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,7 +1,13 @@
+import * as jwt from "koa-jwt";
 import * as miRule from "./miRule";
 import * as bodyParser from "koa-bodyparser";
 
 export default (app) => {
   miRule(app);
   app.use(bodyParser());
+  app.use(koajwt({ secret: SECRET }).unless({
+    // 登录接口不需要验证
+    path: [/^\/api\/login/,/^\api\/register/]
+  }));
+  
 };
